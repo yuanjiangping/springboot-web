b0f65eda1bb8e43fd7291111bf30cade2a3a896b
yuanjiangping <samo_yuan@126.com>
2019-10-18 16:11:45 +0800

    commit by yuanjiangping20191018

--- src/main/java/com/example/springbootweb/config/MyMvcConfig.java
+++ src/main/java/com/example/springbootweb/config/MyMvcConfig.java
@@ -2,11 +2,11 @@ package com.example.springbootweb.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableWebMvc
+
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter{
 
--- src/main/java/com/example/springbootweb/controller/TestController.java
+++ src/main/java/com/example/springbootweb/controller/TestController.java
@@ -3,10 +3,11 @@ package com.example.springbootweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@Controller
-//public class TestController {
-//    @GetMapping("/hello")
-//    public String test(){
-//        return "test";
-//    }
-//}
+@Controller
+public class TestController {
+    @GetMapping("/hello")
+    public String test(){
+        return "test";
+    }
+
+}
