f5d2847156160b3a1bfc981a5d293e16f54e445c
yuanjiangping <samo_yuan@126.com>
2020-07-16 17:35:28 +0800

    方法名添加注释

--- src/main/java/com/example/springbootweb/config/MyMvcConfig.java
+++ src/main/java/com/example/springbootweb/config/MyMvcConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
+/**
+ * @author yuanjiangping
+ */
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter{
 
--- src/main/java/com/example/springbootweb/controller/TestController.java
+++ src/main/java/com/example/springbootweb/controller/TestController.java
@@ -3,11 +3,14 @@ package com.example.springbootweb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @author yuanjiangping
+ */
 @Controller
 public class TestController {
     @GetMapping("/hello")
     public String test(){
-        return "test";
+        return "hello";
     }
 
 }
