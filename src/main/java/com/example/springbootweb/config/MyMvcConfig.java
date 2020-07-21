fe0e3028e4e679c83efe95d2d4dad767cc5b83b3
yuanjiangping <samo_yuan@126.com>
2020-07-21 21:53:43 +0800

    方法名添加注释

--- src/main/java/com/example/springbootweb/config/MyMvcConfig.java
+++ src/main/java/com/example/springbootweb/config/MyMvcConfig.java
@@ -1,8 +1,10 @@
 package com.example.springbootweb.config;
 
 
+import com.example.springbootweb.component.LoginHandlerInterceptor;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,5 +18,12 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("/hello");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
     }
 }
